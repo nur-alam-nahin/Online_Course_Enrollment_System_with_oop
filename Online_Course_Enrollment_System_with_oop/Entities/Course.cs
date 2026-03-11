4ded1dc6bec06843f30be3d63f34ea2103b1d936
nahin <nurealamnahin16@gmail.com>
2026-03-11 11:19:02 +0600

    Add project files.

---
+++ Online_Course_Enrollment_System_with_oop/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Course_Enrollment_System_with_oop.Entities
+{
+    class Course
+    {
+        private string courseTitle;
+        private double baseFee;
+
+
+    }
+}
---
+++ Online_Course_Enrollment_System_with_oop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Course_Enrollment_System_with_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
